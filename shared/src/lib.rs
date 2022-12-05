cd2402019fb8267cda60a4a37846f14daf43704b
Lukas Weber <dev@sakul-flee.de>
2022-12-05 04:56:13 +0100

    Implemented very basic usage

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,6 +1,7 @@
 #![allow(clippy::all)]
 
 use compute_engine::{BaseEngine, ComputeEngine};
+use vulkano::command_buffer::{AutoCommandBufferBuilder, CommandBufferUsage};
 
 #[cfg(test)]
 mod tests;
@@ -9,4 +10,17 @@ pub fn entrypoint() {
     let compute_engine = ComputeEngine::new();
 
     ComputeEngine::print_api_information(compute_engine.get_instance(), log::Level::Info);
+
+    compute_engine.compute(&|engine: &ComputeEngine| {
+        AutoCommandBufferBuilder::primary(
+            engine.get_logical_device().get_device(),
+            engine.get_logical_device().get_queue_family_index(),
+            CommandBufferUsage::OneTimeSubmit,
+        )
+        .unwrap()
+        .build()
+        .unwrap()
+    });
+
+    compute_engine.kill();
 }
